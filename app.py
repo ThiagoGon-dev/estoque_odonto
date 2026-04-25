02681cdc0a889070288ec6fa1fb16ae5f6e4a915
ThiagoGon-dev <eng.thiagogonsan@gmail.com>
2026-04-24 21:37:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
 
@@ -145,4 +146,6 @@ def entrada(id):
 
     return render_template('entrada.html', id=id)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
